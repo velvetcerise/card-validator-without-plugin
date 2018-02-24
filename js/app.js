2fda068f3e79fd56c0c0f1706f125b6e739d443d
velvetcerise <velvet.cerise@hotmail.com>
2018-02-24 00:45:14 +0000

    no sense but teacher wants

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const form = document.querySelector("form");
 let array = Array.from(form)
+//console.log(array); Obtenemos los elementos de form como array
+
 
 form.addEventListener("submit", e => {
   e.preventDefault();
@@ -12,15 +14,27 @@ form.addEventListener("submit", e => {
 
 function validateCardDetails(element) {
   //escribe tu código aqui
-  var total = array.map((uno) => {
+  let total = array.map((uno) => {
+    //arrojando nuevo arrat con los datos del input
     return uno.value;
-  });
+  })
+  //total.forEach((inputValue, whatweGot) =>{
+
+  //});
+
   console.log(total);
   getValues(element)
+
 }
 
-var valueCredit = 
-  console.log(array);
+let valueCard = document.getElementById('cn').value;
+let valueExp = document.getElementById('exp').value;
+let valueCvv = document.getElementById('cvv').value;
+let valueName = document.getElementById('name').value;
+console.log(inputValue);
+
+
   const getValues = (array) => {
+
 
 }
